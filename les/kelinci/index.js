77be5144e82df97225554890cfab11f0067c56d8
farel07 <farrqeel@gmail.com>
2021-12-09 16:56:07 +0700

    initial commit

--- les/kelinci/index.js
+++ les/kelinci/index.js
@@ -1,22 +1,17 @@
-<<<<<<< HEAD
-const burger = document.querySelector(".hamburger");
-const nav = document.querySelector("nav ul");
-const body = document.querySelector("body");
-=======
+
+
 // body
 const body = document.querySelector("body");
 
 const burger = document.querySelector(".hamburger");
 const nav = document.querySelector("nav ul");
->>>>>>> 943dcbb (initial commit)
+
 
 burger.addEventListener("click", function () {
   burger.classList.toggle("transform");
   nav.classList.toggle("slide");
   body.classList.toggle("black");
 });
-<<<<<<< HEAD
-=======
 
 const dropbar = document.querySelector(".dropbar");
 const ulDrop = document.querySelector(".ulDrop");
@@ -35,10 +30,5 @@ profil.addEventListener("mouseleave", function () {
 });
 
 // nav menu
-const menu = document.querySelectorAll(".menu li a");
-menu.forEach(function (menuSemua) {
-  menuSemua.addEventListener("mouseleave", function () {
-    menuSemua.style.transition = "1s";
-  });
-});
->>>>>>> 943dcbb (initial commit)
+
+
--- les/kelinci/index.php
+++ les/kelinci/index.php
@@ -20,15 +20,12 @@ if( isset($_SESSION["login"]) ){
 
 
 
-<<<<<<< HEAD
-?>
-=======
+
 
 ?>
 
 
 
->>>>>>> 943dcbb (initial commit)
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -36,76 +33,7 @@ if( isset($_SESSION["login"]) ){
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Home - LoginLess</title>
-<<<<<<< HEAD
-    <link rel="stylesheet" href="index1.css" />
-    <!-- Bootstrap CSS -->
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" /> -->
-    <!-- end Boostrap CSS -->
-  </head>
-  <body>
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
-    <!-- ----------------------------------------------------------------------------------------------- -->
-    
-    <div class="home">
-      <!-- header -->
-      <div class="header">
-        <h2>Hi' <?php if( isset($_SESSION["login"]) ) : echo $_SESSION["nama"];?> <a href="logout.php">LOG OUT</a></h2><?php 
-        else :
-          echo "customer";
-        ?>
-        <a href="login.php">Login Now</a>
-        <?php endif; ?>
-        <a href="profil.php?id=<?= $akun[0]["id"]; ?>">upload profil</a>
-        <img src="<?= $profil[0]["profil"]; ?>" alt="">
-      </div>
-      <!-- navbar -->
-      <nav>
-        <div class="judul">
-          <h1>LoginLess</h1>
-        </div>
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Product</a></li>
-          <li><a href="#">Galery</a></li>
-          <li><a href="#">Service</a></li>
-          <li><a href="#">About</a></li>
-        </ul>
-        <!-- SEARCH -->
-        <div class="search">
-          <input type="search" placeholder="Search" autofocus />
-        </div>
-        <!-- END SEARCH -->
-        <div class="hamburger">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </nav>
-      <!-- end navbar -->
-      <div class="mulai">
-        <h3>Buy 2 Get Disc 40%</h3>
-        <div class="mulaiBelanja">
-          <h3>Mulai Belanja</h3>
-        </div>
-      </div>
-    </div>
-    <!-- END HOME -->
 
-
-    <?php for( $a = 0; $a < 4; $a++ ) : ?>
-          <div class="produk">
-
-          <ul>
-
-          <img src="<?= $produk[$a]["gambar"]; ?>" width="100px"alt="">
-          <li><?= $produk[$a]["nama_produk"] ?></li>
-          <li><?= $produk[$a]["harga"] ?></li>
-
-    </ul>
-          </div>
-      <?php endfor; ?>
-=======
     <link rel="stylesheet" href="index.css" />
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
@@ -155,7 +83,7 @@ if( isset($_SESSION["login"]) ){
 
     <!-- BOOSTRAP JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
->>>>>>> 943dcbb (initial commit)
+
 
     <script src="index.js"></script>
   </body>
